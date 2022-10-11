07bb23deda57ea09c70d0050daf89a367545bd92
Suraj Sahu <90745657+shaansuraj@users.noreply.github.com>
2022-10-11 21:51:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
 import "./App.css";
+import ScrollToTop from "./ScrollToTop"
 
 function App() {
   return (
+                <ScrollToTop/>
+
     // BEM
     <div className="app">
       <Sidebar />
